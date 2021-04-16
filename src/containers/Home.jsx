2b34eee4536b9036454be41d61e3c385b5c7b3c3
librarylai <library@intella.co>
2021-04-16 23:03:44 +0800

    add init file

---
+++ src/containers/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Home(props) {
+    return (
+        <div>
+            HOME
+        </div>
+    )
+}
+
+Home.propTypes = {
+
+}
+
+export default Home
+
